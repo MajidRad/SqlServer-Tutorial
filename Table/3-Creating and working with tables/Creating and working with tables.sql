7f555b6fe5fd7b2c4ab82b1a94d1107094a4bc39
MajidRad <majidrad@hotmail.com>
2023-01-20 14:33:17 +0330

    adding part3

---
+++ Table/3-Creating and working with tables/Creating and working with tables.sql
@@ -0,0 +1,41 @@
+DROP TABLE IF EXISTS tblPerson;
+DROP TABLE IF EXISTS tblGender;
+DROP DATABASE IF EXISTS Sample;
+Go
+
+
+CREATE DATABASE Sample;
+GO
+
+USE[Sample];
+Go
+
+CREATE TABLE tblGender (
+ID INT NOT NULL PRIMARY KEY IDENTITY,
+Gender NVARCHAR(50) NOT NULL
+)
+GO
+
+INSERT INTO tblGender (Gender)
+VALUES('Male'),('Female'),('Unkown');
+GO
+
+--Froign key naming convention FK_<TARGETTABLE>_<SOURCETABLE> 
+CREATE TABLE tblPerson(
+ID INT NOT NUll IDENTITY,
+Name NVARCHAR(30),
+Email NVARCHAR(30),
+GenderId INT 
+CONSTRAINT PK_Person PRIMARY KEY (ID),
+CONSTRAINT FK_Gender_Person FOREIGN KEY (GenderId) REFERENCES tblGender(ID)
+);
+GO
+
+INSERT INTO tblPerson(Name,Email,GenderId)
+VALUES
+('John','john@test.com',1),
+('Mary','Mary@test.com',2),
+('JohnDo','johnDo@test.com',3),
+('Alex','Alex@test.com',1),
+('Monica','Monica@test.com',2)
+
